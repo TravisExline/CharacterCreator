1f2d64f598ddee5d8bc205d34f4b3cf9824299e1
TravisExline <t.exline22@gmail.com>
2020-06-16 11:17:14 -0500

    initiation styling

--- frontend/src/components/InitiationTrackerForm.js
+++ frontend/src/components/InitiationTrackerForm.js
@@ -20,10 +20,12 @@ class InitiationTrackerForm extends React.Component {
             <div>
                 <NavBar />
                 <h2 className="initiation-header">Initiation Tracker</h2>
+                <div className="initiation-form-holder">
                 <form className="initiation-form" onSubmit={event => this.handleSubmit(event)}>
                     <input className="input-1" type="text" name="characterName" placeholder="Enter Character/NPC Name" onChange={event => this.handleOnChange(event)} value={this.props.characterName}/>
                     <input className="submit-initiation" type="submit" value="Submit Initiation" />
                 </form>
+                </div>
             </div>
         )
     }
