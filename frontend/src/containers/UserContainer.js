25b6cbb9a5078915f9f66b5b4130616f3625d982
TravisExline <t.exline22@gmail.com>
2020-06-19 10:17:02 -0500

    moved to container

--- frontend/src/containers/UserContainer.js
+++ frontend/src/containers/UserContainer.js
@@ -1,19 +1,28 @@
 import React from 'react'
 import User from '../components/User'
 import {connect} from 'react-redux'
+import {fetchCharacters} from '../actions/characterActions'
+import CharacterCard from '../components/CharacterCard'
 
 class UserContainer extends React.Component {
+    
+    componentDidMount() {
+        this.props.fetchCharacters()
+    }
+
     render() {
+        debugger
         return (
             <div>
                 <User username={this.props.user.username}/>
+                {this.props.characters[Math.floor(Math.random() * characters.length)]}
             </div>
         )
     }
 }
 
 const mapStateToProps = (state) => {
-    return {user: state.usersReducer}
+    return {user: state.usersReducer, characters: state.charactersReducer}
 }
 
-export default connect(mapStateToProps)(UserContainer)
+export default connect(mapStateToProps, {fetchCharacters})(UserContainer)
