f26e9b48862331655d1e3313120f688305342b07
TravisExline <t.exline22@gmail.com>
2020-06-19 10:03:51 -0500

    random char on homepage

--- frontend/src/components/User.js
+++ frontend/src/components/User.js
@@ -1,17 +1,32 @@
 import React from 'react'
 import NavBar from './NavBar'
+import {connect} from 'react-redux'
+import {fetchCharacters} from '../actions/characterActions'
+import CharacterCard from '../components/CharacterCard'
 import '../style/User.css'
 
 class User extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchCharacters()
+    }
+
     render() {
         return (
             <div>
                 <h2 className="header">Welcome, {this.props.username}</h2>
                 <NavBar />
+                {this.props.characters.map((character) => (
+                    <CharacterCard key={character.id} character={character} userId={this.props.user.id} handleDestroy={this.props.destroyCharacter}/>
+                ))}
             </div>
             
         )
     }
 }
 
-export default (User)
+const mapStateToProps = (state) => {
+    return {characters: state.charactersReducer, user: state.usersReducer}
+}
+
+export default connect(mapStateToProps, {fetchCharacters})(User)
